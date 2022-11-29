02ff03c2b11c897ba71c4691421308557bd718ec
FourteenD <2899385645@qq.com>
2022-11-29 17:56:55 +0800

    vault backup: 2022-11-29 17:56:55
    Affected files:
    1000-技术/数据接口与算法&设计模式/设计模式/代码示例/Singleton.ts

--- 1000-技术/数据接口与算法&设计模式/设计模式/代码示例/Singleton.ts
+++ 1000-技术/数据接口与算法&设计模式/设计模式/代码示例/Singleton.ts
@@ -1,1 +1,44 @@
-// 单例模式
+// 单例模式的多种实现方式
+// 1. 懒汉式，线程不安全
+class Singleton1 {
+  private static instance: Singleton1;
+  private constructor() { }
+  public static getInstance(): Singleton1 {
+    if (!this.instance) {
+      this.instance = new Singleton1();
+    }
+    return this.instance;
+  }
+}
+// 2. 懒汉式，线程安全
+class Singleton2 {
+  private static instance: Singleton2;
+  private constructor() { }
+  public static getInstance(): Singleton2 {
+    if (!this.instance) {
+      this.instance = new Singleton2();
+    }
+    return this.instance;
+  }
+}
+
+// 3. 饿汉式
+class Singleton3 {
+  private static instance: Singleton3 = new Singleton3();
+  private constructor() { }
+  public static getInstance(): Singleton3 {
+    return this.instance;
+  }
+}
+
+// 4. 双重检查锁
+class Singleton4 {
+  private static instance: Singleton4;
+  private constructor() { }
+  public static getInstance(): Singleton4 {
+    if (!this.instance) {
+      this.instance = new Singleton4();
+    }
+    return this.instance;
+  }
+}
