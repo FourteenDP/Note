0948b4f50009e0cfcf19f87ab230637b3063420a
FourteenD <2899385645@qq.com>
2022-12-08 02:10:03 +0800

    vault backup: 2022-12-08 02:10:02
    Affected files:
    9999-实例/单例模式.ts

--- 9999-实例/单例模式.ts
+++ 9999-实例/单例模式.ts
@@ -1,16 +1,10 @@
 /*
- * @文件路径: \9999-测试\单例模式.ts
+ * @文件路径: \9999-实例\单例模式.ts
  * @创建时间: 2022-12-08 01:38:54
- * @更新时间: 2022-12-08 02:00:29
+ * @更新时间: 2022-12-08 02:08:53
  */
 
 namespace Singleton {
-  /**
-   * @description : 单例模式
-   * @return       {Singleton}
-   * @example     : const instance = Singleton.getInstance();
-   * @note        : 单例模式是指一个类只有一个实例，且该类能自行创建这个实例的一种模式
-   */
   class Singleton {
     private static instance: Singleton;
 
