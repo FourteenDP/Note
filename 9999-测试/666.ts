b97bc98eed404ba73b7f625853bdf33ec8fc7434
FourteenD <2899385645@qq.com>
2022-12-07 23:56:19 +0800

    vault backup: 2022-12-07 23:56:19
    Affected files:
    9999-测试/666.ts

--- 9999-测试/666.ts
+++ 9999-测试/666.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: FourteenD 2899385645@qq.com
+ * @Date: 2022-12-07 22:45:51
+ * @LastEditors: FourteenD 2899385645@qq.com
+ * @LastEditTime: 2022-12-07 23:55:14
+ * @FilePath: \Note\9999-测试\666.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 
 // 冒泡算法
 
