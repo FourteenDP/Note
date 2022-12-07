abac3e154b4ad12b98a47e26bd41158d7a471567
FourteenD <2899385645@qq.com>
2022-12-07 23:18:32 +0800

    vault backup: 2022-12-07 23:18:32
    Affected files:
    9999-测试/test.js

--- 9999-测试/test.js
+++ 9999-测试/test.js
@@ -1,10 +1,9 @@
 // 数组去重
 
 /**
- * @param {Array} arr
- * @return {Array}
- * @description 数组去重
- * @example unique([1, 2, 3, 3, 4, 5, 5, 6]) => [1, 2, 3, 4, 5, 6]
+ *
+ * @param {*} arr
+ * @returns
  */
 function unique(arr) {
   return Array.from(new Set(arr));
