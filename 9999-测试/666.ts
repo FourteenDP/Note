6a0731bb3ea7fd2d51b407fd45fb514cd190538a
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-07 23:52:25 +0800

    vault backup: 2022-12-07 23:52:25
    Affected files:
    9999-测试/666.ts

---
+++ 9999-测试/666.ts
@@ -0,0 +1,15 @@
+// 二叉树
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+// 递归
+var invertTree = function (root) {
+  if (!root) return null;
+  let temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+  invertTree(root.left);
+  invertTree(root.right);
+  return root;
+}
