ed51c19e5ae7681b6782aad561f7f68857cede02
FourteenD <2899385645@qq.com>
2022-12-08 23:47:09 +0800

    vault backup: 2022-12-08 23:47:09
    Affected files:
    z.js

--- z.js
+++ z.js
@@ -1,22 +1,41 @@
 const fs = require('fs')
 const path = require('path')
 
+const dir = path.join(__dirname)
 const tree = {}
 
-function travel(dir, ext, callback) {
-  fs.readdirSync(dir).forEach(function (file) {
-    var pathname = path.join(dir, file)
-    if (fs.statSync(pathname).isDirectory()) {
-      travel(pathname, ext, callback)
+// 遍历目录
+function walk(dir, tree) {
+  const files = fs.readdirSync(dir)
+  files.forEach(file => {
+    const filePath = path.join(dir, file)
+    const stat = fs.statSync(filePath)
+    if (stat.isDirectory()) {
+      tree[file] = {}
+      walk(filePath, tree[file])
     } else {
-      if (path.extname(pathname) === ext) {
-        callback(pathname)
-      }
+      tree[file] = filePath
     }
   })
 }
 
+walk(dir, tree)
 
+// 过滤文件
+function filter(tree, filter) {
+  for (let key in tree) {
+    if (filter(key)) {
+      delete tree[key]
+    } else if (typeof tree[key] === 'object') {
+      filter(tree[key], filter)
+    }
+  }
+}
 
+filter(tree, file => {
+  return file === 'node_modules' || file === 'package-lock.json'
+})
+
+// 输出
 console.log(JSON.stringify(tree, null, 2));
 console.log(tree);
