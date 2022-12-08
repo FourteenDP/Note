6a859c89e5f55780eced2a4945e866dbb9978a27
FourteenD <2899385645@qq.com>
2022-12-09 01:09:42 +0800

    up

--- tree.js
+++ tree.js
@@ -38,8 +38,76 @@ function filter(tree, prefixs = ['.', '_']) {
 
 filter(tree, ['.', '_', 'tree.js', '~', '-', '0000'])
 
+// 包含文件夹和文件
+function filterMd(tree) {
+  for (let key in tree) {
+    if (typeof tree[key] === 'object') {
+      filterMd(tree[key])
+    } else {
+      if (!key.endsWith('.md')) {
+        delete tree[key]
+      }
+    }
+  }
+}
+
+filterMd(tree)
+
+// tree对象 转换为child node数组
+function treeToArray(tree) {
+  const arr = []
+  for (let key in tree) {
+    if (typeof tree[key] === 'object') {
+      arr.push({
+        title: key,
+        children: treeToArray(tree[key])
+      })
+    } else {
+      arr.push({
+        title: key,
+        path: tree[key]
+      })
+    }
+  }
+  return arr
+}
+
+const arr = treeToArray(tree)
+
+// child node数组 转 markdown 格式
+function arrayToMd(arr, title = '📋目录') {
+  let md = `---
+title: ${title}
+aliases:
+tags:
+  - 目录
+date created: ${new Date().toISOString().slice(0, 10) + ' ' + new Date().toISOString().slice(11, 19)}
+date updated: ${new Date().toISOString().slice(0, 10) + ' ' + new Date().toISOString().slice(11, 19)}
+---
+
+# ${title}
+
+`
+  arr.forEach(item => {
+    if (item.children) {
+      md += `- **[[${item.title}]]**\n`
+    } else {
+      md += `- [[${item.title}]]\n`
+    }
+  })
+  return md
+}
 
 
-// 输出
-console.log(JSON.stringify(tree, null, 2));
-console.log(tree);
+// 给每个文件夹生成目录
+function generateMd(arr, dir) {
+  arr.forEach(item => {
+    if (item.children) {
+      const md = arrayToMd(item.children, item.title)
+      fs.writeFileSync(path.join(dir, item.title, '📋目录.md'), md)
+      generateMd(item.children, path.join(dir, item.title))
+    }
+  })
+}
+
+generateMd(arr, dir)
