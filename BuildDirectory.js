a835df4c96bf1805640d84d52cccdd600070c843
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-08 16:48:27 +0800

    vault backup: 2022-12-08 16:48:27
    Affected files:
    BuildDirectory.js

--- BuildDirectory.js
+++ BuildDirectory.js
@@ -1,26 +1,17 @@
 /*
  * @文件路径: \BuildDirectory.js
  * @创建时间: 2022-12-08 16:40:53
- * @更新时间: 2022-12-08 16:45:35
+ * @更新时间: 2022-12-08 16:47:24
  */
 
 // 打印目录文件
 const fs = require('fs');
 
-// 递归打印目录
-function printDirectory(path) {
-  let files = fs.readdirSync(path + '/'); // 读取目录
-  files.forEach(function (item, index) {
-    let fPath = path + '/' + item;
-    let stat = fs.statSync(fPath);
-    if (stat.isDirectory() === true) {
-      printDirectory(fPath);
-    }
-    if (stat.isFile() === true) {
-      console.log(fPath);
-    }
-  });
-}
+// 读取目录
+const files = fs.readdirSync('./');
 
-// 打印目录文件
-printDirectory(__dirname + '/1000-技术');
+// 打印目录
+
+files.forEach((file) => {
+  console.log(file);
+});
