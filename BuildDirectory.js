e3e587858efe4f7eae4c244e79c2b99669339cae
FourteenD <2899385645@qq.com>
2022-12-08 17:18:54 +0800

    vault backup: 2022-12-08 17:18:54
    Affected files:
    BuildDirectory.js
    DrerectoryTree.txt

--- BuildDirectory.js
+++ BuildDirectory.js
@@ -1,7 +1,7 @@
 /*
  * @文件路径: \BuildDirectory.js
  * @创建时间: 2022-12-08 16:40:53
- * @更新时间: 2022-12-08 17:10:23
+ * @更新时间: 2022-12-08 17:17:37
  */
 
 
@@ -10,24 +10,40 @@ const fs = require('fs')
 const path = require('path')
 
 
-// 生成文件树
-function buildDirectoryTree(dir, tree = {}) {
+/**
+ * @description: 生成文件树过滤器
+ * @param {string} dir
+ * @param {Array} filter
+ * @param {string} type
+ * @return {Object}
+ * @example: buildDirectoryTree('./', ['.js', '.json'], 'include')
+*/
+
+function buildDirectoryTree(dir, filter, type) {
   const files = fs.readdirSync(dir)
-  files.forEach(file => {
-    if (file.startsWith('.') || file.startsWith('-') || file.startsWith('~')) return
+  const result = {}
+  files.forEach((file) => {
+    // 过滤隐藏文件
+    if (file.startsWith('.')) return;
     const filePath = path.join(dir, file)
     const stats = fs.statSync(filePath)
-    if (stats.isDirectory(filePath)) {
-      tree[file] = {}
-      buildDirectoryTree(filePath, tree[file])
-    }
-    else {
-      tree[file] = null
+    if (stats.isDirectory()) {
+      result[file] = buildDirectoryTree(filePath, filter, type)
+    } else if (stats.isFile()) {
+      if (type === 'include') {
+        if (filter.includes(path.extname(filePath))) {
+          result[file] = filePath
+        }
+      } else if (type === 'exclude') {
+        if (!filter.includes(path.extname(filePath))) {
+          result[file] = filePath
+        }
+      }
     }
   })
-  return tree
+  return result
 }
 
-// 将文件树打印到DrerectoryTree.txt
 
-fs.writeFileSync('DrerectoryTree.txt', JSON.stringify(buildDirectoryTree('./'), null, 2))
+// 将文件树打印到DrerectoryTree.txt
+fs.writeFileSync('DrerectoryTree.txt', JSON.stringify(buildDirectoryTree('./', ['.js', '.json'], 'exclude'), null, 2))
