753225fafa3ab6dc88f09caa33154b1abf16525e
FourteenD <2899385645@qq.com>
2022-12-08 20:36:27 +0800

    vault backup: 2022-12-08 20:36:26
    Affected files:
    BuildDirectory.js

--- BuildDirectory.js
+++ BuildDirectory.js
@@ -1,7 +1,7 @@
 /*
  * @文件路径: \BuildDirectory.js
  * @创建时间: 2022-12-08 16:40:53
- * @更新时间: 2022-12-08 20:32:41
+ * @更新时间: 2022-12-08 20:35:22
  */
 
 
@@ -55,6 +55,7 @@ function treeToMarkdown(tree, level = 0) {
     } else if (key === 'README.md' || key === 'DrerectoryTree.md') {
       return
     } else {
+      level === 0 ? result += `# ${key}\n` :
       result += `${'  '.repeat(level)}- ${key}\n`
       if (typeof tree[key] === 'object') {
         result += treeToMarkdown(tree[key], level + 1)
