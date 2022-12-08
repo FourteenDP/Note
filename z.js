358ac0cabcaaf9dd41b1207acad4c8277e329c4a
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-08 23:42:08 +0800

    vault backup: 2022-12-08 23:42:08
    Affected files:
    z.js

--- z.js
+++ z.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
+const tree = {}
+
 function travel(dir, ext, callback) {
   fs.readdirSync(dir).forEach(function (file) {
     var pathname = path.join(dir, file)
@@ -14,72 +16,7 @@ function travel(dir, ext, callback) {
   })
 }
 
-let tree = {}
 
-travel('./', '.md', function (pathname) {
-  // 用\分割路径，然后用转成tree
-  let arr = pathname.split('\\')
-  let temp = tree
-  arr.forEach((item, index) => {
-    if (index === arr.length - 1) {
-      temp[item] = true
-    } else {
-      if (!temp[item]) {
-        temp[item] = {}
-      }
-      temp = temp[item]
-    }
-  })
-})
-
-// 排序 tree 优先级 文件夹 > 文件 > 符号 > 数字 > 字母 > 其他
-function sortTree(tree) {
-  let keys = Object.keys(tree)
-  keys.sort((a, b) => {
-    if (tree[a] === true && tree[b] !== true) {
-      return 1
-    } else if (tree[a] !== true && tree[b] === true) {
-      return -1
-    } else if (tree[a] === true && tree[b] === true) {
-      return a.localeCompare(b)
-    } else {
-      return a.localeCompare(b)
-    }
-  })
-  let temp = {}
-  keys.forEach((item) => {
-    if (tree[item] !== true) {
-      temp[item] = sortTree(tree[item])
-    } else {
-      temp[item] = true
-    }
-  })
-  return temp
-}
-
-tree = sortTree(tree)
-
-// 过滤匹配的文件夹和文件
-function filterTree(tree, filter) {
-  let keys = Object.keys(tree)
-  let temp = {}
-  keys.forEach((item) => {
-    if (tree[item] !== true) {
-      let res = filterTree(tree[item], filter)
-      if (Object.keys(res).length > 0) {
-        temp[item] = res
-      }
-    } else {
-      if (!filter.test(item)) {
-        temp[item] = true
-      }
-    }
-  })
-  return temp
-}
-
-// 过滤掉以 '. 开头的文件夹和文件
-tree = filterTree(tree, /^\./)
 
 console.log(JSON.stringify(tree, null, 2));
 console.log(tree);
