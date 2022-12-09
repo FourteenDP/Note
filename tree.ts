0eb9745638ca9f3cf783cc42a264dab7a55fd3aa
FourteenD <2899385645@qq.com>
2022-12-09 23:12:31 +0800

    vault backup: 2022-12-09 23:12:31
    Affected files:
    tree.json
    tree.ts
    📋目录.md

--- tree.ts
+++ tree.ts
@@ -1,28 +1,72 @@
-namespace Tree {
-  // 遍历文件夹
-  class Tree {
-    constructor() {
-      const fs = require('fs');
-      const path = require('path');
-      const dir = path.resolve(__dirname, '../');
-      const files = fs.readdirSync(dir);
-      this.traverse(files, dir);
-    }
+const fs: any = require('fs');
+const path: any = require('path');
+export enum FilterType {
+  include,
+  exclude
+}
 
-    traverse(files, dir) {
-      files.forEach((file) => {
-        const filePath = path.resolve(dir, file);
-        const stats = fs.statSync(filePath);
-        if (stats.isDirectory()) {
-          const files = fs.readdirSync(filePath);
-          this.traverse(files, filePath);
-        } else {
-          console.log(filePath);
-        }
-      });
+export class Tree {
+  private root: string;
+  private tree: any;
 
-    }
+  constructor(root: string) {
+    this.root = root;
+    this.tree = {};
+  }
+  public getTree(type?: FilterType, filter?: (file: string) => boolean) {
+    this.tree = this.getDir(this.root);
+    if (type && filter) this.tree = this.filterTree(this.tree, type, filter);
+    fs.writeFileSync('./tree.json', JSON.stringify(this.tree, null, 2));
+    return this.tree;
+  }
+  private getDir(dir: string) {
+    const tree: any = {};
+    const files: string[] = fs.readdirSync(dir);
+    files.forEach((file: string) => {
+      const filePath: string = path.join(dir, file);
+      const stat = fs.statSync(filePath);
+      if (stat.isDirectory()) {
+        tree[file] = this.getDir(filePath);
+      }
+      if (stat.isFile()) {
+        tree[file] = filePath;
+      }
+    });
+    return tree;
   }
 
-  const tree = new Tree();
+  public filterTree(tree: any, type: FilterType, filter: (file: string) => boolean) {
+    const result: any = {};
+    Object.keys(tree).forEach((key: string) => {
+      const value: any = tree[key];
+      if (typeof value === 'string') {
+        if (type === FilterType.include) {
+          if (filter(value)) {
+            result[key] = value;
+          }
+        } else {
+          if (!filter(value)) {
+            result[key] = value;
+          }
+        }
+      } else {
+        const subTree: any = this.filterTree(value, type, filter);
+        if (Object.keys(subTree).length > 0) {
+          result[key] = subTree;
+        }
+      }
+    });
+    return result;
+  }
 }
+const tree = new Tree('./');
+tree.getTree(FilterType.exclude, (file: string) => {
+  let boolean = false;
+  const startWiths = ['.', '-', '~', '0000'];
+  startWiths.forEach((char: string) => {
+    if (file.startsWith(char)) {
+      boolean = true;
+    }
+  });
+  return boolean;
+});
