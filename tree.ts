e194dab87d25be73c8891f27cba91c31990d242e
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-09 21:42:52 +0800

    up

--- tree.ts
+++ tree.ts
@@ -1,7 +1,28 @@
 namespace Tree {
-    export class Tree {
-        constructor() {
-            console.log('Tree');
-        }
+  // 遍历文件夹
+  class Tree {
+    constructor() {
+      const fs = require('fs');
+      const path = require('path');
+      const dir = path.resolve(__dirname, '../');
+      const files = fs.readdirSync(dir);
+      this.traverse(files, dir);
     }
+
+    traverse(files, dir) {
+      files.forEach((file) => {
+        const filePath = path.resolve(dir, file);
+        const stats = fs.statSync(filePath);
+        if (stats.isDirectory()) {
+          const files = fs.readdirSync(filePath);
+          this.traverse(files, filePath);
+        } else {
+          console.log(filePath);
+        }
+      });
+
+    }
+  }
+
+  const tree = new Tree();
 }
