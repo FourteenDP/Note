3d8a31fa6b91f86e324a99467d34f503cf6d1f95
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-12 16:31:03 +0800

    vault backup: 2022-12-12 16:31:02
    Affected files:
    1000-技术/2100-前端开发/Vue/Vue3/Demo/src/App.tsx
    1000-技术/2100-前端开发/Vue/Vue3/Demo/src/router/index.ts

--- 1000-技术/2100-前端开发/Vue/Vue3/Demo/src/router/index.ts
+++ 1000-技术/2100-前端开发/Vue/Vue3/Demo/src/router/index.ts
@@ -1,23 +1,8 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
-const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/views/Home'),
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('@/views/About.vue'),
-  },
-  // 404
-  {
-    path: '/:pathMatch(.*)*',
-    name: 'NotFound',
-    component: () => import('@/views/NotFound.vue'),
-  },
-]
+
+
+const routes: RouteRecordRaw[] = []
 
 const router = createRouter({
   history: createWebHistory(),
