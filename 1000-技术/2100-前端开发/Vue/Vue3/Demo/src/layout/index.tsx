aba80e7d86c8b1399ca78715a0caa0b4f3ac990f
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-12 17:46:39 +0800

    vault backup: 2022-12-12 17:46:39
    Affected files:
    .obsidian/plugins/obsidian-activity-history/data.json
    1000-技术/2100-前端开发/Vue/Vue3/Demo/src/layout/index.tsx
    1000-技术/2100-前端开发/Vue/Vue3/Demo/src/router/index.ts
    1000-技术/2100-前端开发/Vue/Vue3/Demo/src/store/index.ts
    1000-技术/2100-前端开发/Vue/Vue3/Demo/笔记/使用vite搭建vue3项目.md

--- 1000-技术/2100-前端开发/Vue/Vue3/Demo/src/layout/index.tsx
+++ 1000-技术/2100-前端开发/Vue/Vue3/Demo/src/layout/index.tsx
@@ -1,17 +1,12 @@
 import { defineComponent } from "vue";
 
-const className = {
-  marginRight: "20px",
-};
-
 export default defineComponent({
   name: "Layout",
   setup() {
     return () => (
       <div class="layout">
         <p>
-          <router-link class={className} to="/home">首页</router-link>
-
+          <router-link to="/home">首页</router-link>
           <router-link to="/about">关于</router-link>
         </p>
         <router-view />
