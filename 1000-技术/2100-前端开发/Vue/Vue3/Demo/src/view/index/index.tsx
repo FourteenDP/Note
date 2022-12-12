42ce46f5aefc55b59e949183065b834e4343cb62
FourteenD <2899385645@qq.com>
2022-12-12 16:19:46 +0800

    vault backup: 2022-12-12 16:19:45
    Affected files:
    .obsidian/plugins/obsidian-activity-history/data.json
    1000-技术/2100-前端开发/Vue/Vue3/Demo/pnpm-lock.yaml
    1000-技术/2100-前端开发/Vue/Vue3/Demo/src/App.tsx
    1000-技术/2100-前端开发/Vue/Vue3/Demo/src/App.vue
    1000-技术/2100-前端开发/Vue/Vue3/Demo/src/assets/vue.svg
    1000-技术/2100-前端开发/Vue/Vue3/Demo/src/components/HelloWorld.vue
    1000-技术/2100-前端开发/Vue/Vue3/Demo/src/main.ts
    1000-技术/2100-前端开发/Vue/Vue3/Demo/src/router/index.ts
    1000-技术/2100-前端开发/Vue/Vue3/Demo/src/style.css
    1000-技术/2100-前端开发/Vue/Vue3/Demo/src/view/index/index.tsx
    1000-技术/2100-前端开发/Vue/Vue3/Demo/笔记/使用vite搭建vue3项目.md

---
+++ 1000-技术/2100-前端开发/Vue/Vue3/Demo/src/view/index/index.tsx
@@ -0,0 +1,12 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Index",
+  setup() {
+    return () => (
+      <div>
+        <h1>hello vue3</h1>
+      </div>
+    );
+  }
+});
