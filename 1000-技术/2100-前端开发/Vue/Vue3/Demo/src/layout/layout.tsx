5807006cecfdcc9c7c78bb322eb85a10480f142e
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-12 17:28:27 +0800

    vault backup: 2022-12-12 17:28:27
    Affected files:
    1000-技术/2100-前端开发/Vue/Vue3/Demo/src/layout/layout.tsx

--- 1000-技术/2100-前端开发/Vue/Vue3/Demo/src/layout/layout.tsx
+++ 1000-技术/2100-前端开发/Vue/Vue3/Demo/src/layout/layout.tsx
@@ -5,7 +5,12 @@ export default defineComponent({
   setup() {
     return () => (
       <div class="layout">
+        <p>
+          <router-link to="/">Go to Home</router-link>
+          <router-link to="/about">Go to About</router-link>
+        </p>
         <router-view />
+
       </div>
     );
   },
