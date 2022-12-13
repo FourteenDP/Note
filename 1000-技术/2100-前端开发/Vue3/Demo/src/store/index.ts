1143c8917763711887f62b3b8888a2a9c3de527f
FourteenD <2899385645@qq.com>
2022-12-13 12:26:15 +0800

    vault backup: 2022-12-13 12:26:14
    Affected files:
    1000-技术/2100-前端开发/Vue3/Demo/src/store/index.ts

--- 1000-技术/2100-前端开发/Vue3/Demo/src/store/index.ts
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/store/index.ts
@@ -1,3 +1,10 @@
 import { createPinia } from 'pinia'
 
 export const pinia = createPinia()
+
+// 休眠
+export async function sleep(ms: number) {
+   await setTimeout(() => {
+      return true
+   }, ms)
+}
