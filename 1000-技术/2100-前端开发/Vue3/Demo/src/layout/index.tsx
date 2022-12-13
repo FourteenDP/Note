27a9990488f7ff109b2f51c37ed154aa7398597a
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-13 12:52:19 +0800

    vault backup: 2022-12-13 12:52:18
    Affected files:
    1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx

--- 1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx
@@ -4,7 +4,7 @@ export default defineComponent({
   name: "Layout",
   setup() {
     return () => (
-      <div class="layout">
+      <div class="layou">
         <p>
           <router-link to="/">首页</router-link>
           <router-link to="/about">关于</router-link>
