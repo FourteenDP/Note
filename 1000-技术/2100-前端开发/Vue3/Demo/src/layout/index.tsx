5b2a3df1b86049ca5324369602e8bc28fc8f5317
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-13 10:33:38 +0800

    up

--- 1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx
@@ -6,7 +6,7 @@ export default defineComponent({
     return () => (
       <div class="layout">
         <p>
-          <router-link to="/home">首页</router-link>
+          <router-link to="/">首页</router-link>
           <router-link to="/about">关于</router-link>
         </p>
         <router-view />
