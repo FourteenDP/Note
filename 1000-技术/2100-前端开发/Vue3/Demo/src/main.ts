1d406b20297ba92ef0d59ed9afb923a8f14896d6
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-13 12:11:06 +0800

    up

--- 1000-技术/2100-前端开发/Vue3/Demo/src/main.ts
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App'
 import router from './router'
-import './styles/styles.styl'
+import './styles/styles.css'
 
 const app = createApp(App)
 app.use(router)
