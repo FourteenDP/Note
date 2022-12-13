a707afdd522252bb59ea6ca871f9679d990bf25b
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-13 17:58:35 +0800

    vault backup: 2022-12-13 17:58:34
    Affected files:
    1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx

--- 1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx
@@ -1,6 +1,8 @@
 import { defineComponent } from "vue";
 import { useRouter } from 'vue-router'
 import "./index.module.styl";
+
+
 export default defineComponent({
   name: "Layout",
   setup() {
@@ -9,17 +11,19 @@ export default defineComponent({
     const menuRoutes = routes.filter(route => {
       return route.meta?.showMenu
     })
+    const menu = menuRoutes.map(route => {
+      return (
+        <div class="menu-item" onClick={() => router.push(route.path)}>
+          {route.meta?.title}
+        </div>
+      )
+    })
+
 
     return () => (
       <div class="layou">
-        <div class="menu fixed bottom-0 left-1/2">
-          {menuRoutes.map(route => {
-            return (
-              <div class="menu-item" onClick={() => router.push(route.path)}>
-                {route.meta?.title}
-              </div>
-            )
-          })}
+        <div class="menu fixed  bottom-0 left-1/2 -translate-x-1/2 bg-white flex flex-row">
+          {menu}
         </div>
         <router-view />
       </div>
