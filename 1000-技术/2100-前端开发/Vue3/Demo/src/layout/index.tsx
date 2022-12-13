1c5e3bfb88e99a192997370951a4094c234ee687
FourteenD <2899385645@qq.com>
2022-12-13 16:19:50 +0800

    vault backup: 2022-12-13 16:19:49
    Affected files:
    .obsidian/plugins/obsidian-activity-history/data.json
    1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx

--- 1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx
@@ -10,13 +10,8 @@ export default defineComponent({
     })
     console.log(menuRoutes);
 
-
     return () => (
       <div class="layou">
-        <p>
-          <router-link to="/">首页</router-link>
-          <router-link to="/about">关于</router-link>
-        </p>
         <router-view />
       </div>
     );
