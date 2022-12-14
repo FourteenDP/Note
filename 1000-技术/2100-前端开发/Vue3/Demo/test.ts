10678652e1c6ed01985d6f1f67829ddce932cf54
FourteenD <2899385645@qq.com>
2022-12-14 22:18:18 +0800

    vault backup: 2022-12-14 22:18:17
    Affected files:
    1000-技术/2100-前端开发/Vue3/Demo/test.ts

---
+++ 1000-技术/2100-前端开发/Vue3/Demo/test.ts
@@ -0,0 +1,29 @@
+function foo() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve
+    }, 1000)
+  })
+}
+
+function bar() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve
+    }, 1000)
+  })
+}
+
+foo.bar = bar;
+// 描述A的类型
+interface A {
+  // 对象key
+  [key: string]: {
+    (): Promise<unknown>
+    [key:string]: ()=>Promise<unknown>
+  }
+}
+
+let a: A = {
+  foo
+}
