c14e8b6e3e2f9ee6331015fc706a17b4bf2d5d83
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-14 20:08:35 +0800

    vault backup: 2022-12-14 20:08:34
    Affected files:
    .obsidian/plugins/obsidian-activity-history/data.json
    1000-技术/2100-前端开发/Vue3/Demo/tailwind.config.cjs

--- 1000-技术/2100-前端开发/Vue3/Demo/tailwind.config.cjs
+++ 1000-技术/2100-前端开发/Vue3/Demo/tailwind.config.cjs
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['./src/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: [],
+  content: [
+    './src/**/*.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
