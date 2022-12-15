86f65fa8bfd0c96e78d8d0b06a5fd0172c94ce2f
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-15 21:46:39 +0800

    vault backup: 2022-12-15 21:46:38
    Affected files:
    1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/LifecycleHooks.tsx

--- 1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/LifecycleHooks.tsx
+++ 1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/LifecycleHooks.tsx
@@ -1,8 +1,4 @@
-import { defineComponent, onBeforeMount, onMounted } from "vue";
-
-interface LifecycleHooksProps {
-  msg: string;
-}
+import { defineComponent, onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onRenderTracked, onRenderTriggered, onUnmounted, onUpdated } from "vue";
 
 export default defineComponent({
   name: "LifecycleHooks",
@@ -10,6 +6,10 @@ export default defineComponent({
     title: "生命周期钩子",
   },
   setup() {
+    let count = $ref(0)
+    const increment = () => {
+      count++
+    }
     // 实例挂载之前
     onBeforeMount(() => {
       console.log("onBeforeMount");
@@ -19,15 +19,33 @@ export default defineComponent({
       console.log("created");
     });
     // 实例卸载之前
-    beforeUnmount(() => {
-      console.log("beforeUnmount");
+    onBeforeUnmount(() => {
+      console.log("onBeforeUnmount");
     });
-
-
-
+    // 实例卸载之后
+    onUnmounted(() => {
+      console.log("onUnmounted");
+    });
+    // 数据更新之前
+    onBeforeUpdate(() => {
+      console.log("onBeforeUpdate");
+    });
+    // 数据更新之后
+    onUpdated(() => {
+      console.log("onUpdated");
+    });
+    // 跟踪响应式数据的变化
+    onRenderTracked((event) => {
+      console.log("onRenderTracked", event);
+    });
+    // 触发响应式数据的变化
+    onRenderTriggered((event) => {
+      console.log("onRenderTriggered", event);
+    });
     return () => (
       <div>
-
+        <div class="text-xl">count: {count}</div>
+        <button class="btn btn-primary" onClick={increment}>+1</button>
       </div>
     );
   },
