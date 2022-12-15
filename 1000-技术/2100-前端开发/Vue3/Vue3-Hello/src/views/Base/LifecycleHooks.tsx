0290b5e31a57206326e0e205ded575eb6413a021
FourteenD <2899385645@qq.com>
2022-12-15 21:49:03 +0800

    vault backup: 2022-12-15 21:49:02
    Affected files:
    1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/LifecycleHooks.tsx

--- 1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/LifecycleHooks.tsx
+++ 1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/LifecycleHooks.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onRenderTracked, onRenderTriggered, onUnmounted, onUpdated } from "vue";
+import { defineComponent, onActivated, onBeforeMount, onBeforeUnmount, onBeforeUpdate, onDeactivated, onMounted, onRenderTracked, onRenderTriggered, onUnmounted, onUpdated } from "vue";
 
 export default defineComponent({
   name: "LifecycleHooks",
@@ -12,35 +12,43 @@ export default defineComponent({
     }
     // 实例挂载之前
     onBeforeMount(() => {
-      console.log("onBeforeMount");
+      console.log("实例挂载之前onBeforeMount");
     });
     // 实例挂载之后
     onMounted(() => {
-      console.log("created");
+      console.log("实例挂载之后created");
     });
     // 实例卸载之前
     onBeforeUnmount(() => {
-      console.log("onBeforeUnmount");
+      console.log("实例卸载之前onBeforeUnmount");
     });
     // 实例卸载之后
     onUnmounted(() => {
-      console.log("onUnmounted");
+      console.log("实例卸载之后onUnmounted");
     });
     // 数据更新之前
     onBeforeUpdate(() => {
-      console.log("onBeforeUpdate");
+      console.log("数据更新之前onBeforeUpdate");
     });
     // 数据更新之后
     onUpdated(() => {
-      console.log("onUpdated");
+      console.log("数据更新之后onUpdated");
     });
     // 跟踪响应式数据的变化
     onRenderTracked((event) => {
-      console.log("onRenderTracked", event);
+      console.log("跟踪响应式数据的变化onRenderTracked", event);
     });
     // 触发响应式数据的变化
     onRenderTriggered((event) => {
-      console.log("onRenderTriggered", event);
+      console.log("触发响应式数据的变化onRenderTriggered", event);
+    });
+    // 路由激活时
+    onActivated(() => {
+      console.log("路由激活时onActivated");
+    });
+    // 路由离开时
+    onDeactivated(() => {
+      console.log("路由离开时onDeactivated");
     });
     return () => (
       <div>
