a6858861325a050c2f0847bd4c4f9bf0b75de4b8
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-15 11:21:55 +0800

    vault backup: 2022-12-15 11:21:54
    Affected files:
    1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx

--- 1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx
@@ -1,29 +1,34 @@
 import { defineComponent, } from "vue";
+import { useRouter } from "vue-router";
 import { routes } from '@/router'
-import { RouterLink } from "vue-router";
 type RouteRecordRaw = import('vue-router').RouteRecordRaw
-function getMneus(routes: RouteRecordRaw[]) {
-  return routes.map(item => {
-    if (item.children) {
-      return (
-        <div class="dropdown dropdown-bottom">
-          <label tabindex="0" class="btn m-1">{item.meta?.title}</label>
-          <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-            {getMneus(item.children)}
-          </ul>
-        </div>
-      )
-    } else {
-      // return <li><a>{item.meta?.title}</a></li>
-      return <li><a> <router-link to={item.path}>{item.meta?.title}</router-link></a></li>
-    }
-  })
-}
-console.log(getMneus(routes[0].children!));
+
 
 export default defineComponent({
   name: "Layout",
   setup() {
+    const route = useRouter()
+    console.log(route);
+
+
+    function getMneus(routes: RouteRecordRaw[]) {
+      return routes.map(item => {
+        if (item.children) {
+          return (
+            <div class="dropdown dropdown-bottom">
+              <label tabindex="0" class="btn m-1">{item.meta?.title}</label>
+              <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                {getMneus(item.children)}
+              </ul>
+            </div>
+          )
+        } else {
+          return <li><a onClick={() => {
+
+          }}>{item.meta?.title}</a></li>
+        }
+      })
+    }
     return () => (
       <div class="layou">
         <div class="menu bg-primary textw flex flex-row p-1">
