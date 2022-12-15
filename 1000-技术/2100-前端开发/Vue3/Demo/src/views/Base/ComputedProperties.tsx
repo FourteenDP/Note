ded9ac38f007088639a2693ad5e32970022212a5
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-15 13:28:43 +0800

    vault backup: 2022-12-15 13:28:42
    Affected files:
    1000-技术/2100-前端开发/Vue3/Demo/src/router/index.ts
    1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/ComputedProperties.tsx

--- 1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/ComputedProperties.tsx
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/ComputedProperties.tsx
@@ -6,9 +6,18 @@ export default defineComponent({
     title: "计算属性",
   },
   setup() {
-    const state = $ref{
-      count: 0,
-    }
+    const state = $ref([
+      { name: "Alice", age: 20 },
+      { name: "Bob", age: 30 },
+      { name: "Carol", age: 40 },
+    ])
+    // 平均年龄
+    const averageAge = $computed(() => {
+      return state.reduce((sum, person) => sum + person.age, 0) / state.length
+    })
+
+    console.log(averageAge);
+
     return () => (
       <div>
         <div class="text-xl">计算属性</div>
