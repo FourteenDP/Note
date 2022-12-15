40a978c00fb7d72640905a34ada12caf5a7b0445
FourteenD <2899385645@qq.com>
2022-12-15 17:07:09 +0800

    up

--- 1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/ComputedProperties.tsx
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/ComputedProperties.tsx
@@ -1,46 +1,40 @@
-import { unlock } from "@/utils";
 import { defineComponent } from "vue";
 
-interface UserInfo {
-  name: string
-  age: number
-}
 export default defineComponent({
   name: "ComputedProperties",
   meta: {
     title: "计算属性",
   },
   setup() {
-    const state: UserInfo[] = $ref([])
+    // 计算属性
+    let count = $ref(0)
 
-    const addUserInfo: UserInfo = $ref({
-      name: '',
-      age: 0
-    })
-    // 平均年龄
-    const averageAge = $computed(() => {
-      return state.reduce((sum, person) => sum + person.age, 0) / state.length
+    const double = $computed(() => count * 2)
+
+    const increment = () => {
+      count++
+    }
+
+    // 可写计算属性
+    let count2 = $ref(0)
+
+    let double2 = $computed({
+      get: () => count2 * 2,
+      set: (val) => {
+        count2 = val / 2
+      },
     })
 
     return () => (
       <div>
-        <div class="text-xl">平均年龄: {averageAge}</div>
-        <div class="text-xl">用户信息: {JSON.stringify(state)}</div>
-        <div class="form-control">
-          <label class="label">
-            <span class="label-text">姓名</span>
-          </label>
-          <input class="input input-bordered" type="text" v-model={addUserInfo.name} />
-          <label class="label">
-            <span class="label-text">年龄</span>
-          </label>
-          <input class="input input-bordered" type="number" v-model={addUserInfo.age} />
-        </div>
-        <button class="btn btn-primary" onClick={() => {
-          state.push({ ...addUserInfo })
-          addUserInfo.name = ''
-          addUserInfo.age = 0
-        }}>添加</button>
+        <div class="text-xl">count: {count}</div>
+        <button class="btn btn-primary" onClick={increment}>+1</button>
+        <div class="text-xl">double: {double}</div>
+        <h2>可写计算属性</h2>
+        <div class="text-xl">count2: {count2}</div>
+        <button class="btn btn-primary" onClick={() => count2++}>+1</button>
+        <div class="text-xl">double2: {double2}</div>
+        <button class="btn btn-primary" onClick={() => double2 = 10}>double2 = 10</button>
       </div>
     );
   },
