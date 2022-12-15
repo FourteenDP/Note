8332c8b2ec0165722e3097421d89ee48c75b7fb8
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-15 15:44:12 +0800

    vault backup: 2022-12-15 15:44:12
    Affected files:
    1000-技术/2100-前端开发/Vue3/Demo/src/router/index.ts
    1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/ComputedProperties.tsx

--- 1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/ComputedProperties.tsx
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/ComputedProperties.tsx
@@ -11,13 +11,31 @@ export default defineComponent({
       { name: "Bob", age: 30 },
       { name: "Carol", age: 40 },
     ])
+
+    const addUserInfo = $ref({ name: "", age: 0 })
     // 平均年龄
     const averageAge = $computed(() => {
       return state.reduce((sum, person) => sum + person.age, 0) / state.length
     })
     return () => (
       <div>
-        <div class="text-xl">计算属性</div>
+        <div class="text-xl">平均年龄: {averageAge}</div>
+        <div class="text-xl">用户信息: {JSON.stringify(state)}</div>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">姓名</span>
+          </label>
+          <input class="input input-bordered" type="text" v-model={addUserInfo.name} />
+          <label class="label">
+            <span class="label-text">年龄</span>
+          </label>
+          <input class="input input-bordered" type="text" v-model={addUserInfo.age} />
+        </div>
+        <button class="btn btn-primary" onClick={() => {
+          state.push(addUserInfo)
+          addUserInfo.name = ''
+          addUserInfo.age = 0
+        }}>添加</button>
       </div>
     );
   },
