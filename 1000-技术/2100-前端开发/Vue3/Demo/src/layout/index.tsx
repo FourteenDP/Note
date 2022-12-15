f3efea6c8e9820ab0612fe69c7f08eda109c32b0
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-15 11:08:49 +0800

    vault backup: 2022-12-15 11:08:48
    Affected files:
    1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx

--- 1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/layout/index.tsx
@@ -1,20 +1,31 @@
 import { defineComponent, } from "vue";
 import { routes } from '@/router'
-console.log(routes);
-const menu = routes.filter((route) => route.meta?.title);
+type RouteRecordRaw = import('vue-router').RouteRecordRaw
+function getMneus(routes: RouteRecordRaw[]) {
+  return routes.map(item => {
+    if (item.children) {
+      return (
+        <div class="dropdown dropdown-bottom">
+          <label tabindex="0" class="btn m-1">{item.meta?.title}</label>
+          <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+            {getMneus(item.children)}
+          </ul>
+        </div>
+      )
+    } else {
+      return <li><a>{item.meta?.title}</a></li>
+    }
+  })
+}
+console.log(getMneus(routes[0].children!));
+
 export default defineComponent({
   name: "Layout",
   setup() {
     return () => (
       <div class="layou">
-        <div class="menu bg-primary textw flex flex-row p-2">
-          <div class="dropdown dropdown-bottom">
-            <label tabindex="0" class="btn m-1">Click</label>
-            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-              <li><a>Item 1</a></li>
-              <li><a>Item 2</a></li>
-            </ul>
-          </div>
+        <div class="menu bg-primary textw flex flex-row p-1">
+          {getMneus(routes[0].children!)}
         </div>
       </div>
     );
