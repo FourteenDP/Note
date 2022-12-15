ab15b7475fcff036934bc6351f5214d9dc57d60c
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-15 17:07:09 +0800

    up

---
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/ClassAndStyleBindings.ts
@@ -0,0 +1,14 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ClassAndStyleBindings",
+  meta: {
+    title: "Class与Style绑定",
+  },
+  data() {
+    return {
+      isActive: true,
+      error: null,
+    };
+  }
+})
