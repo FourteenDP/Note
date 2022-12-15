fe1baffc806f825af4f0ef15bf2b261851dce119
FourteenD <2899385645@qq.com>
2022-12-15 20:04:54 +0800

    up

--- 1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/ConditionalAndListRendering.tsx
+++ 1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/ConditionalAndListRendering.tsx
@@ -6,37 +6,6 @@ export default defineComponent({
     title: "条件和列表渲染",
   },
   setup() {
-    // 条件渲染
-    let show = $ref(true)
-    const toggle = () => {
-      show = !show
-    }
-
-    // v-if
-    let show2 = $ref(true)
-    const toggle2 = () => {
-      show2 = !show2
-    }
-
-    // v-else
-    let show3 = $ref(true)
-    const toggle3 = () => {
-      show3 = !show3
-    }
-
-    // v-else-if
-    let show4 = $ref(1)
-    const toggle4 = () => {
-      show4 = show4 + 1
-    }
-
-    // v-show
-    let show5 = $ref(true)
-    const toggle5 = () => {
-      show5 = !show5
-    }
-
-    // v-if + v-for
     let todos = $ref([
       { text: "Learn JavaScript" },
       { text: "Learn Vue" },
@@ -48,36 +17,20 @@ export default defineComponent({
 
     return () => (
       <div>
-        <div class="text-xl">show: {show ? "true" : "false"}</div>
-        <button class="btn btn-primary" onClick={toggle}>toggle</button>
-        <div class="text-xl">show2: {show2 ? "true" : "false"}
-          <div v-if={show2}>v-if</div>
-        </div>
-        <button class="btn btn-primary" onClick={toggle2}>toggle2</button>
-        <div class="text-xl">show3: {show3 ? "true" : "false"}
-          <div v-if={show3}>v-if</div>
-          <div v-else>v-else</div>
-        </div>
-        <button class="btn btn-primary" onClick={toggle3}>toggle3</button>
-        <div class="text-xl">show4: {show4}
-          <div v-if={show4 === 1}>v-if</div>
-          <div v-else-if={show4 === 2}>v-else-if</div>
-          <div v-else>v-else</div>
-        </div>
-        <button class="btn btn-primary" onClick={toggle4}>toggle4</button>
-        <div class="text-xl">show5: {show5 ? "true" : "false"}
-          <div v-show={show5}>v-show</div>
-        </div>
-        <button class="btn btn-primary" onClick={toggle5}>toggle5</button>
-        <div class="text-xl">v-if + v-for</div>
+        {/* 条件渲染 */}
+        {todos.length > 0 ? <div>有待办事项</div> : <div>没有待办事项</div>}
+        {/* 列表渲染 */}
         <ul>
           {todos.map((todo, index) => (
             <li key={index}>
               {todo.text}
-              <button class="btn btn-primary" onClick={() => remove(index)}>remove</button>
+              <button class={['btn']} onClick={() => remove(index)}>X</button>
             </li>
           ))}
         </ul>
+        <div v-show={todos.length}>
+          <button class={['btn']}>添加</button>
+        </div>
       </div>
     );
   },
