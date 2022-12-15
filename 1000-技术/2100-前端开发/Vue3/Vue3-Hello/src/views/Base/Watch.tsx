298225a5a47a987919dfef5185cc97f75b2a230c
FourteenD <2899385645@qq.com>
2022-12-15 22:14:17 +0800

    vault backup: 2022-12-15 22:14:17
    Affected files:
    1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/Watch.tsx

--- 1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/Watch.tsx
+++ 1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/Watch.tsx
@@ -26,12 +26,30 @@ export default defineComponent({
       console.log(`count changed: ${oldVal2} -> ${newVal2}`)
     })
 
+    // 深度监听
+    const deepState = reactive({ count: 0, obj: { a: 1 } })
+    watch(deepState, (newVal, oldVal) => {
+      console.log(`deepState changed: ${oldVal} -> ${newVal}`)
+    }, { deep: true })
+
+    // getter 监听
+    const getterState = reactive({ count: 0 })
+    watch(() => getterState.count + 10, (newVal, oldVal) => {
+      console.log(`getterState changed: ${oldVal} -> ${newVal}`)
+    })
+
     return () => (
       <div>
         <div class="text-xl">count: {count.value}</div>
         <button class="btn btn-primary" onClick={increment}>+1</button>
         <div class="text-xl">state.count: {state.count}</div>
         <button class="btn btn-primary" onClick={() => state.count++}>+1</button>
+        <div class="text-xl">deepState.count: {deepState.count}</div>
+        <button class="btn btn-primary" onClick={() => deepState.count++}>+1</button>
+        <div class="text-xl">deepState.obj.a: {deepState.obj.a}</div>
+        <button class="btn btn-primary" onClick={() => deepState.obj.a++}>+1</button>
+        <div class="text-xl">getterState.count: {getterState.count}</div>
+        <button class="btn btn-primary" onClick={() => getterState.count++}>+1</button>
       </div>
     );
   },
