8e6c945389f7eba8d148f98130564f3e924e5520
FourteenD <2899385645@qq.com>
2022-12-15 23:13:44 +0800

    vault backup: 2022-12-15 23:13:44
    Affected files:
    1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/TemplateRefs.tsx

--- 1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/TemplateRefs.tsx
+++ 1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/TemplateRefs.tsx
@@ -1,9 +1,25 @@
-import { defineComponent } from "vue";
+import { defineComponent, nextTick, onMounted, ref } from "vue";
 export default defineComponent({
   name: "TemplateRefs",
   meta: {
     title: "模板引用",
   },
   setup() {
+    // 声明一个 ref 来存放该元素的引用
+    // 必须和模板里的 ref 同名
+    const inputRef = ref<HTMLInputElement | null>(null)
+
+    onMounted(() => {
+      // 在 mounted 时，获取该元素的引用
+      // 通过 ref.value 获取
+      nextTick(() => {
+        console.log(inputRef);
+      })
+    })
+    return () => (
+      <div>
+        <input ref="inputRef" />
+      </div>
+    )
   }
 })
