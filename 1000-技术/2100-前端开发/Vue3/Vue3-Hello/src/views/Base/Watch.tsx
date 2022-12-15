0ed6aa7f627512234143505fd3b07347eb584774
FourteenD <2899385645@qq.com>
2022-12-15 22:09:43 +0800

    vault backup: 2022-12-15 22:09:43
    Affected files:
    1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/Watch.tsx

--- 1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/Watch.tsx
+++ 1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/Watch.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, watch } from "vue";
+import { defineComponent, reactive, ref, watch } from "vue";
 
 export default defineComponent({
   name: "Watch",
@@ -6,18 +6,32 @@ export default defineComponent({
     title: "监听器",
   },
   setup() {
-    let count = $ref(0)
+    let count = ref(0)
     const increment = () => {
-      count++
+      count.value++
     }
-    watch(count, async (nV, oV) => {
-      console.log("count变化了", nV, oV);
+    // 监听ref count的变化
+    watch(count, (newVal, oldVal) => {
+      console.log(`count changed: ${oldVal} -> ${newVal}`)
+    })
+
+    // 监听reactive count的变化
+    const state = reactive({ count: 0 })
+    watch(() => state.count, (newVal, oldVal) => {
+      console.log(`state.count changed: ${oldVal} -> ${newVal}`)
+    })
+    // 监听多个值
+    watch([() => state.count, count], ([newVal1, newVal2], [oldVal1, oldVal2]) => {
+      console.log(`state.count changed: ${oldVal1} -> ${newVal1}`)
+      console.log(`count changed: ${oldVal2} -> ${newVal2}`)
     })
 
     return () => (
       <div>
-        <div class="text-xl">count: {count}</div>
+        <div class="text-xl">count: {count.value}</div>
         <button class="btn btn-primary" onClick={increment}>+1</button>
+        <div class="text-xl">state.count: {state.count}</div>
+        <button class="btn btn-primary" onClick={() => state.count++}>+1</button>
       </div>
     );
   },
