650bfd577a8c7682df2ca515cc822a34bd2c9b34
FourteenD <2899385645@qq.com>
2022-12-15 12:08:54 +0800

    vault backup: 2022-12-15 12:08:54
    Affected files:
    .obsidian/plugins/obsidian-activity-history/data.json
    1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/Reactivity.tsx
    1000-技术/2100-前端开发/Vue3/Demo/笔记/ref和reactive.md
    1000-技术/2100-前端开发/Vue3/Demo/笔记/ref和reactive区别.md
    1000-技术/2100-前端开发/Vue3/Demo/笔记/📋目录.md

--- 1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/Reactivity.tsx
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/Reactivity.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, reactive } from "vue";
+import { defineComponent, reactive, ref } from "vue";
 
 
 export default defineComponent({
@@ -7,16 +7,21 @@ export default defineComponent({
     title: "响应式",
   },
   setup() {
+    // reactive() 方法返回一个响应式的代理对象
     const state = reactive({ count: 0 })
     function increment() {
       state.count++
     }
 
+    // ref() 方法返回一个响应式的代理对象
+    const count = ref(0)
 
     return () => (
       <div>
-        <div class="text-xl">count: {state.count}</div>
+        <div class="text-xl">state: {state.count}</div>
         <button class="btn btn-primary" onClick={increment}>+1</button>
+        <div class="text-xl">ref: {count.value}</div>
+        <button class="btn btn-primary" onClick={() => count.value++}>+1</button>
       </div>
     );
   },
