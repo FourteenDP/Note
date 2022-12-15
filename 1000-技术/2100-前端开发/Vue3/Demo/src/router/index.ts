fab1f758ea6ef9a978eb642df3b58e19b5657289
FourteenD <2899385645@qq.com>
2022-12-15 14:02:10 +0800

    vault backup: 2022-12-15 14:02:10
    Affected files:
    1000-技术/2100-前端开发/Vue3/Demo/src/router/index.ts

--- 1000-技术/2100-前端开发/Vue3/Demo/src/router/index.ts
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/router/index.ts
@@ -5,51 +5,29 @@ export const routes: RouteRecordRaw[] = [
     path: '/',
     name: 'Layout',
     component: () => import('@/layout'),
-    children: [
-      {
-        path: '/Base',
-        name: 'Base',
-        meta: (await import('@/views/Base')).default.meta,
-        component: () => import('@/views/Base'),
-        children: [
-          {
-            path: '/Base/Reactivity',
-            name: 'Reactivity',
-            meta: (await import('@/views/Base/Reactivity')).default.meta,
-            component: () => import('@/views/Base/Reactivity'),
-          },
-        ]
-      },
-      {
-        path: '/Component',
-        name: 'Component',
-        meta: (await import('@/views/Component')).default.meta,
-        component: () => import('@/views/Component'),
-        children: [
-          {
-            path: '/Component/ComponentRegistration',
-            name: 'ComponentRegistration',
-            meta: (await import('@/views/Component/ComponentRegistration')).default.meta,
-            component: () => import('@/views/Component/ComponentRegistration'),
-          },
-        ]
-      },
-    ],
+    children: [],
   },
 ]
 
 // 自动导入路由
 const files = import.meta.glob('../views/**/*.tsx')
-
-routes[0].children?.forEach((item) => {
-  Object.keys(files).forEach((key) => {
-    const path = key.replace('../views', '').replace('.tsx', '')
-    const name = path.replace('/', '').split('/')
-    console.log(name);
-
-
-  })
+Object.keys(files).forEach((key) => {
+  const path = key.replace('../views', '').replace('.tsx', '')
+  const name = path.replace('/', '').split('/')
+  if (name.length === 1) {
+    routes[0].children?.push({
+      path,
+      name: name[0],
+      component: files[key],
+    })
+    console.log({
+      path,
+      name: name[0],
+      component: files[key],
+    });
+  }
 })
+
 
 const router = createRouter({
   history: createWebHistory(),
