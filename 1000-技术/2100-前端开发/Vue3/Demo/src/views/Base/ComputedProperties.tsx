31078a53eacb16a904ec31eb53d54bb2fea802ce
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-15 16:01:31 +0800

    vault backup: 2022-12-15 16:01:31
    Affected files:
    1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/ComputedProperties.tsx

--- 1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/ComputedProperties.tsx
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/ComputedProperties.tsx
@@ -12,7 +12,7 @@ export default defineComponent({
       { name: "Carol", age: 40 },
     ])
 
-    const addUserInfo = $ref({ name: "", age: 0 })
+    const addUserInfo ={ name: "", age: 0 }
     // 平均年龄
     const averageAge = $computed(() => {
       return state.reduce((sum, person) => sum + person.age, 0) / state.length
@@ -32,8 +32,8 @@ export default defineComponent({
           <input class="input input-bordered" type="text" v-model={addUserInfo.age} />
         </div>
         <button class="btn btn-primary" onClick={() => {
-          state.push(toRaw(addUserInfo))
-          console.log(toRaw(addUserInfo));
+          state.push(addUserInfo)
+          console.log(addUserInfo);
 
         }}>添加</button>
       </div>
