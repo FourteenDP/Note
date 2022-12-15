9daf4adbc45b5584dbc3de874432931499782c6c
FourteenD <2899385645@qq.com>
2022-12-15 13:22:51 +0800

    vault backup: 2022-12-15 13:22:51
    Affected files:
    .obsidian/plugins/obsidian-activity-history/data.json
    1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/ComputedProperties.tsx

--- 1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/ComputedProperties.tsx
+++ 1000-技术/2100-前端开发/Vue3/Demo/src/views/Base/ComputedProperties.tsx
@@ -6,7 +6,9 @@ export default defineComponent({
     title: "计算属性",
   },
   setup() {
-
+    const state = $ref{
+      count: 0,
+    }
     return () => (
       <div>
         <div class="text-xl">计算属性</div>
