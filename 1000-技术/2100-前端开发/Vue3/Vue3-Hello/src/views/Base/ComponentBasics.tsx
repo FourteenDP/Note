3b434d9a15ef5c4800f8ad49c058759a5ad76624
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-16 11:29:15 +0800

    vault backup: 2022-12-16 11:29:14
    Affected files:
    1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/ComponentBasics.tsx

--- 1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/ComponentBasics.tsx
+++ 1000-技术/2100-前端开发/Vue3/Vue3-Hello/src/views/Base/ComponentBasics.tsx
@@ -6,17 +6,19 @@ export default defineComponent({
     title: '组件基础',
   },
   setup() {
-    return () => <div class={['flex']}>
-      <div class="mockup-phone">
-        <div class="camera"></div>
-        <div class="display">
-          <div class="artboard artboard-demo phone-1">
-            我是父组件
+    return () => (
+      <div class='flex'>
+        <div class="mockup-phone">
+          <div class="camera"></div>
+          <div class="display">
+            <div class="artboard artboard-demo phone-1">
+              我是父组件
+            </div>
           </div>
         </div>
+        <A />
       </div>
-      <A />
-    </div>;
+    );
   },
 });
 
