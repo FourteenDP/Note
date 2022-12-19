4984f9691a2a66f2fb74a8dbcdfcab48509ebc95
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2022-12-19 11:31:18 +0800

    up

--- 1000-技术/2100-前端开发/Vue3/Vue3-Hello/types/env.d.ts
+++ 1000-技术/2100-前端开发/Vue3/Vue3-Hello/types/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="vue/macros-global" />
+import 'vue-router'
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    title?: string
+    showMenu?: boolean,
+    icon?: string,
+  }
+}
