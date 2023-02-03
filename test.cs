ffebefcfebcc2cf13a1cc3502893139b188dfc3d
FourteenD <2899385645@qq.com>
2023-02-03 22:18:31 +0800

    vault backup: 2023-02-03 22:18:31
    Affected files:
    test.cs

--- test.cs
+++ test.cs
@@ -1,16 +1,27 @@
-class Singleton
+class Helpers
 {
-    private static Singleton instance = null;
-    private Singleton() { }
-    public static Singleton Instance
+    private static Helpers instance = null;
+    private Helpers() { }
+    public static Helpers Instance
     {
         get
         {
             if (instance == null)
             {
-                instance = new Singleton();
+                instance = new Helpers();
             }
             return instance;
         }
+    }
+
+    // diff array
+    public int[] Diff(int[] a, int[] b)
+    {
+        int[] result = new int[a.Length];
+        for (int i = 0; i < a.Length; i++)
+        {
+            result[i] = a[i] - b[i];
+        }
+        return result;
     }
 }
