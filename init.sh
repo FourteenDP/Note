93818a878857596556418f7bda267527766a0468
FourteenD <2899385645@qq.com>
2023-02-05 00:10:16 +0800

    up

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 git subtree add --prefix=拾肆 git@github.com:FourteenD/-.git main --squash
+git subtree add --prefix=仓库/Vue3-Hello git@github.com:FourteenD/Vue3-Hello.git main --squash
+git subtree add --prefix=仓库/Script git@github.com:FourteenD/Script.git main --squash
+git subtree add --prefix=仓库/Code-Snippets git@github.com:FourteenD/Code-Snippets.git main --squash
+git subtree add --prefix=仓库/CS-Hello git@github.com:FourteenD/CS-Hello.git main --squash
