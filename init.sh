11d86fde6458a1dd16de1530f1d5d53ed28e1743
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2023-02-05 01:23:54 +0800

    up

--- init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git submodule add git@github.com:FourteenD/-.git 拾肆
-git submodule add git@github.com:FourteenD/Vue3-Hello.git 仓库/Vue3-Hello
-git submodule add git@github.com:FourteenD/Script.git 仓库/Script
-git submodule add git@github.com:FourteenD/Code-Snippets.git 仓库/Code-Snippets
-git submodule add git@github.com:FourteenD/CS-Hello.git 仓库/CS-Hello
