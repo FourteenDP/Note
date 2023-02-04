08cacc0c34e26afcea3bdbf2caca2a1613b5a62c
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2023-02-05 00:29:00 +0800

    vault backup: 2023-02-05 00:29:00
    Affected files:
    .obsidian/plugins/obsidian-activity-history/data.json
    init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+git remote add subtree-- git@github.com:FourteenD/-.git
+git remote add subtree-Vue3-Hello git@github.com:FourteenD/Vue3-Hello.git
+git remote add subtree-Script git@github.com:FourteenD/Script.git
+git remote add subtree-Code-Snippets git@github.com:FourteenD/Code-Snippets.git
+git remote add subtree-CS-Hello git@github.com:FourteenD/CS-Hello.git
+
 git subtree add --prefix=拾肆 git@github.com:FourteenD/-.git main --squash
 git subtree add --prefix=仓库/Vue3-Hello git@github.com:FourteenD/Vue3-Hello.git main --squash
 git subtree add --prefix=仓库/Script git@github.com:FourteenD/Script.git main --squash
