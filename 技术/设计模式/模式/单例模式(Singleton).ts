8a33ed80fdca0e88a4e1e43dcaa4b91dddf609f9
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2023-03-02 16:04:24 +0800

    vault backup: 2023-03-02 16:04:11
    Affected files:
    .obsidian/plugins/obsidian-activity-history/data.json
    技术/设计模式/模式/单例模式(Singleton).md
    技术/设计模式/模式/单例模式(Singleton).ts

--- 技术/设计模式/模式/单例模式(Singleton).ts
+++ 技术/设计模式/模式/单例模式(Singleton).ts
@@ -1,4 +1,5 @@
 namespace Singleton {
+  // 懒汉式
   class Singleton {
     private static instance: Singleton;
 
@@ -11,9 +12,38 @@ namespace Singleton {
       return Singleton.instance;
     }
   }
-
-  // 测试
   const instance = Singleton.getInstance();
   const instance2 = Singleton.getInstance();
   console.log(instance === instance2);
+
+  // 饿汉式
+  class Singleton2 {
+    private static instance: Singleton2 = new Singleton2();
+
+    private constructor() { }
+
+    static getInstance(): Singleton2 {
+      return Singleton2.instance;
+    }
+  }
+  const instance3 = Singleton2.getInstance();
+  const instance4 = Singleton2.getInstance();
+  console.log(instance3 === instance4);
+
+  // 登记式/静态内部类
+  class Singleton3 {
+    private static instance: Singleton3;
+
+    private constructor() { }
+
+    static getInstance(): Singleton3 {
+      if (!Singleton3.instance) {
+        Singleton3.instance = new Singleton3();
+      }
+      return Singleton3.instance;
+    }
+  }
+  const instance5 = Singleton3.getInstance();
+  const instance6 = Singleton3.getInstance();
+  console.log(instance5 === instance6);
 }
