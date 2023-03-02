db2aed128d47380b75bded291e57fa7023797966
FourteenD <2899385645@qq.com>
2023-03-02 21:26:37 +0800

    vault backup: 2023-03-02 21:26:33
    Affected files:
    -附件-/Pasted image 20230302212615.png
    .gitignore
    .obsidian/plugins/obsidian-activity-history/data.json
    技术/设计模式/模式/Program.cs
    技术/设计模式/模式/单例模式(Singleton).cs
    技术/设计模式/模式/单例模式(Singleton).md

--- 技术/设计模式/模式/单例模式(Singleton).cs
+++ 技术/设计模式/模式/单例模式(Singleton).cs
@@ -1,10 +1,78 @@
-namespace Singleton {
+namespace Singleton
+{
   // 饿汉式
-  public class Singleton1 {
+  public class Singleton1
+  {
     private static Singleton1 instance = new Singleton1();
     private Singleton1() { }
-    public static Singleton1 Instance {
-      get { return instance; }
+    public static Singleton1 Instance
+    {
+      get
+      {
+        return instance;
+      }
+    }
+  }
+
+  // 懒汉式
+  public class Singleton2
+  {
+    private static Singleton2 instance = null;
+    private Singleton2() { }
+    public static Singleton2 Instance
+    {
+      get
+      {
+        if (instance == null)
+        {
+          instance = new Singleton2();
+        }
+        return instance;
+      }
+    }
+  }
+
+  // 双重检查锁定
+  public class Singleton3
+  {
+    private static Singleton3 instance = null;
+    private static readonly object padlock = new object();
+    Singleton3() { }
+    public static Singleton3 Instance
+    {
+      get
+      {
+        if (instance == null)
+        {
+          lock (padlock)
+          {
+            if (instance == null)
+            {
+              instance = new Singleton3();
+            }
+          }
+        }
+        return instance;
+      }
+    }
+  }
+
+
+  // 测试
+  public class Test
+  {
+    public static void Main()
+    {
+      Singleton1 s1 = Singleton1.Instance;
+      Singleton1 sl1 = Singleton1.Instance;
+      Singleton2 s2 = Singleton2.Instance;
+      Singleton2 sl2 = Singleton2.Instance;
+      Singleton3 s3 = Singleton3.Instance;
+      Singleton3 sl3 = Singleton3.Instance;
+
+      System.Console.WriteLine(s1 == sl1);
+      System.Console.WriteLine(s2 == sl2);
+      System.Console.WriteLine(s3 == sl3);
     }
   }
 }
