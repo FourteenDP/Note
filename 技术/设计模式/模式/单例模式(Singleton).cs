fe81a58d9c2d803ebad6c4c3a7c475c6dbbe5877
FourteenD <2899385645@qq.com>
2023-03-02 21:44:38 +0800

    vault backup: 2023-03-02 21:44:37
    Affected files:
    技术/设计模式/模式/单例模式(Singleton).cs

--- 技术/设计模式/模式/单例模式(Singleton).cs
+++ 技术/设计模式/模式/单例模式(Singleton).cs
@@ -1,10 +1,10 @@
 namespace Singleton
 {
   // 饿汉式
-  public class Singleton1
+  public sealed class Singleton1
   {
-    private static Singleton1 instance = new Singleton1();
     private Singleton1() { }
+    private static Singleton1 instance = new Singleton1();
     public static Singleton1 Instance
     {
       get
