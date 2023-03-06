f8c82dfc56d64df5686616aa4e7b241efa033ff3
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2023-03-07 03:56:44 +0800

    vault backup: 2023-03-07 03:56:43
    Affected files:
    技术/设计模式/模式/函数柯里化(Curry).md
    技术/设计模式/模式/函数柯里化(Curry).ts

--- 技术/设计模式/模式/函数柯里化(Curry).ts
+++ 技术/设计模式/模式/函数柯里化(Curry).ts
@@ -1,11 +1,11 @@
 namespace Curry {
   /**
-   * @description : 函数柯里化
+   * 函数柯里化
    * @param        {Function} fn
    * @param        {Array} args
    * @return       {Function}
-   * @example     : curry(add, 1, 2, 3, 4, 5) || curry(add, 1, 2)(3, 4, 5) || curry(add, 1)(2)(3)(4)(5)
-   * @note        : 柯里化是将使用多个参数的一个函数转换成一系列使用一个参数的函数的技术
+  * @example: curry(add, 1, 2, 3, 4, 5) || curry(add, 1, 2) (3, 4, 5) || curry(add, 1) (2) (3) (4) (5)
+  * @note: **函数柯里化**是把接受多个参数的函数变换成接受一个单一参数（最初函数的第一个参数）的函数，并且返回接受余下的参数而且返回结果的新函数的技术。
    */
   function curry<T>(fn: Function, ...args: T[]): Function {
     return args.length >= fn.length
