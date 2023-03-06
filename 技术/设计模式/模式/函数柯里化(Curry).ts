dee32b7f3e798daf8bd79655564ff8d39b7e03ea
FourteenD <2899385645@qq.com>
2023-03-07 03:49:49 +0800

    vault backup: 2023-03-07 03:49:45
    Affected files:
    技术/设计模式/模式/函数柯里化(Curry).ts

--- 技术/设计模式/模式/函数柯里化(Curry).ts
+++ 技术/设计模式/模式/函数柯里化(Curry).ts
@@ -7,17 +7,17 @@ namespace Curry {
    * @example     : curry(add, 1, 2, 3, 4, 5) || curry(add, 1, 2)(3, 4, 5) || curry(add, 1)(2)(3)(4)(5)
    * @note        : 柯里化是将使用多个参数的一个函数转换成一系列使用一个参数的函数的技术
    */
-  function curry(fn: Function, ...args: any[]): Function {
+  function curry<T>(fn: Function, ...args: T[]): Function {
     return args.length >= fn.length
       ? fn(...args)
-      : (..._args) => curry(fn, ...args, ..._args);
+F: (..._args: T[]) => curry(fn, ...args, ..._args);
   }
 
   function add(a: number, b: number): number {
     return a + b;
   }
 
-  console.log(curry(add, 1, 2, 3, 4, 5));
-  console.log(curry(add, 1, 2)(3, 4, 5));
-  console.log(curry(add, 1)(2)(3)(4)(5));
+  console.log(curry<number>(add, 1, 2, 3, 4, 5));
+  console.log(curry<number>(add, 1, 2)(3, 4, 5));
+  console.log(curry<number>(add, 1)(2)(3)(4)(5));
 }
