a48394e71fa66e81528a7a5827d91392d7cf0f26
FourteenD <2899385645@qq.com>
2023-03-08 20:20:01 +0800

    vault backup: 2023-03-08 20:20:00
    Affected files:
    技术/设计模式/GoF的23种设计模式/设计模式分类/创建型模式(5种)/工厂方法模式(Factory Method).js

--- 技术/设计模式/GoF的23种设计模式/设计模式分类/创建型模式(5种)/工厂方法模式(Factory Method).js
+++ 技术/设计模式/GoF的23种设计模式/设计模式分类/创建型模式(5种)/工厂方法模式(Factory Method).js
@@ -1,1 +1,30 @@
-// 工厂模式
+// 工厂模式 Factory Method
+
+// 简单工厂模式势力
+
+class Product {
+  constructor() {
+    this.name = 'product'
+  }
+  init() {
+    console.log('init')
+  }
+  fun1() {
+    console.log('fun1')
+  }
+  fun2() {
+    console.log('fun2')
+  }
+}
+
+class Creator {
+  create(name) {
+    return new Product(name)
+  }
+}
+
+// test
+let creator = new Creator()
+let p = creator.create('p1')
+p.init()
+p.fun1()
