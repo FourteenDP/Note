127eda4235ce33b4ffb57f2a5ca30e63c283a7bd
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2023-03-08 20:34:12 +0800

    vault backup: 2023-03-08 20:34:09
    Affected files:
    技术/设计模式/GoF的23种设计模式/设计模式分类/创建型模式(5种)/工厂方法模式(Factory Method).ts

--- 技术/设计模式/GoF的23种设计模式/设计模式分类/创建型模式(5种)/工厂方法模式(Factory Method).ts
+++ 技术/设计模式/GoF的23种设计模式/设计模式分类/创建型模式(5种)/工厂方法模式(Factory Method).ts
@@ -2,10 +2,9 @@ namespace DesignPatterns.FactoryMethod {
   interface Product {
     use(): void;
     getOwner(): string;
-
   }
 
-  class Product implements Product {
+  class IDCard implements Product {
     private owner: string;
     constructor(owner: string) {
       console.log(`制作${owner}的ID卡`);
@@ -18,4 +17,36 @@ namespace DesignPatterns.FactoryMethod {
       return this.owner;
     }
   }
+
+  abstract class Factory {
+    create(owner: string): Product {
+      const p = this.createProduct(owner);
+      this.registerProduct(p);
+      return p;
+    }
+    protected abstract createProduct(owner: string): Product;
+    protected abstract registerProduct(product: Product): void;
+  }
+
+  class IDCardFactory extends Factory {
+    private owners: string[] = [];
+    protected createProduct(owner: string): Product {
+      return new IDCard(owner);
+    }
+    protected registerProduct(product: Product): void {
+      this.owners.push(product.getOwner());
+    }
+    getOwners(): string[] {
+      return this.owners;
+    }
+  }
+
+  const factory = new IDCardFactory();
+  const card1 = factory.create('张三');
+  const card2 = factory.create('李四');
+  const card3 = factory.create('王五');
+  card1.use();
+  card2.use();
+  card3.use();
+  console.log(factory.getOwners());
 }
