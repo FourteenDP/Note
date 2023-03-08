30c5f89c91f6e3131b3a2c789b39e9b2fa0e6921
FourteenD <2899385645@qq.com>
2023-03-08 19:22:56 +0800

    vault backup: 2023-03-08 19:22:55
    Affected files:
    技术/设计模式/GoF的23种设计模式/设计模式分类/创建型模式(5种)/工厂方法模式(Factory Method).ts

--- 技术/设计模式/GoF的23种设计模式/设计模式分类/创建型模式(5种)/工厂方法模式(Factory Method).ts
+++ 技术/设计模式/GoF的23种设计模式/设计模式分类/创建型模式(5种)/工厂方法模式(Factory Method).ts
@@ -1,4 +1,15 @@
 namespace DesignPatterns.FactoryMethod {
-  // 工厂
-
+  // 产品
+  interface Product {
+    use(): void;
+  }
+  abstract class Factory {
+    public create(owner: string): Product {
+      const product = this.createProduct(owner);
+      this.registerProduct(product);
+      return product;
+    }
+    protected abstract createProduct(owner: string): Product;
+    protected abstract registerProduct(product: Product): void;
+  }
 }
