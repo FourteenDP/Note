a35e5cf35a5e754f8d159d03456887069b7c0074
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2023-03-10 04:08:11 +0800

    vault backup: 2023-03-10 04:08:10
    Affected files:
    技术/设计模式/非GoF设计模式/防抖与节流模式(Debounce & Throttle).ts

--- 技术/设计模式/非GoF设计模式/防抖与节流模式(Debounce & Throttle).ts
+++ 技术/设计模式/非GoF设计模式/防抖与节流模式(Debounce & Throttle).ts
@@ -10,11 +10,11 @@ namespace DesignPatterns.DebounceAndThrottle {
    * const debounceFn = debounce(fn, 1000) // 一秒内多次触发, 只执行最后一次
    * window.addEventListener('scroll', debounceFn)
    */
-  export function debounce(fn: Function, delay: number, immediate: boolean = false) {
+  export function debounce<T>(fn: T, delay: number, immediate: boolean = false) {
     let timer: any = null
-    return function <T>(...args: any[]) {
+    return function (this:ThisParameterType<T>, ...args: any[]) {
       // 给this绑定一个变量, 防止在setTimeout中this指向window
-      const self = this as T
+      const self = this
       if (timer) clearTimeout(timer)
       if (immediate) {
         if (!timer) fn.apply(self, args)
@@ -31,5 +31,4 @@ namespace DesignPatterns.DebounceAndThrottle {
   const fn = () => console.log('fn')
   const debounceFn = debounce(fn, 1000)
   setInterval(debounceFn, 100)
-
 }
