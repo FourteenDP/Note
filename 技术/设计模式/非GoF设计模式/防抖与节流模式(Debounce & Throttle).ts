ccdfa0209fec5dedab9b41e27ff7693c5a717024
FourteenD <2899385645@qq.com>
2023-03-10 02:52:56 +0800

    vault backup: 2023-03-10 02:52:55
    Affected files:
    技术/设计模式/非GoF设计模式/防抖与节流模式(Debounce & Throttle).ts

--- 技术/设计模式/非GoF设计模式/防抖与节流模式(Debounce & Throttle).ts
+++ 技术/设计模式/非GoF设计模式/防抖与节流模式(Debounce & Throttle).ts
@@ -6,7 +6,27 @@ namespace DesignPatterns.DebounceAndThrottle {
    * @param fn 要执行的函数
    * @param delay 延迟时间
    * @param immediate 是否立即执行
+   * @example
+   * const fn = debounce(() => console.log('debounce'), 1000)
    */
-
   export function debounce(fn: Function, delay: number, immediate: boolean = false) {
+    // 定时器
+    let timer: number | null = null
+
+    return function (...args: any[]) {
+      if (timer) clearTimeout(timer)
+
+      if (immediate) {
+        const callNow = !timer
+        timer = setTimeout(() => {
+          timer = null
+        }, delay)
+        if (callNow) fn.apply(this, args)
+      } else {
+        timer = setTimeout(() => {
+          fn.apply(this, args)
+        }, delay)
+      }
+    }
+  }
 }
