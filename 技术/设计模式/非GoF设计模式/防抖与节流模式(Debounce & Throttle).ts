34ea0139be6c1418f1edc8e7d9f81e78cba48d13
FourteenD <2899385645@qq.com>
2023-03-10 02:49:26 +0800

    vault backup: 2023-03-10 02:49:25
    Affected files:
    技术/数据结构与算法/LRU算法.ts
    技术/设计模式/非GoF设计模式/防抖与节流模式(Debounce & Throttle).ts

---
+++ 技术/设计模式/非GoF设计模式/防抖与节流模式(Debounce & Throttle).ts
@@ -0,0 +1,12 @@
+namespace DesignPatterns.DebounceAndThrottle {
+  // 防抖与节流模式
+
+  /**
+   * 防抖 (debounce) 模式： 在一定时间内，只执行一次
+   * @param fn 要执行的函数
+   * @param delay 延迟时间
+   * @param immediate 是否立即执行
+   */
+
+  export function debounce(fn: Function, delay: number, immediate: boolean = false) {
+}
