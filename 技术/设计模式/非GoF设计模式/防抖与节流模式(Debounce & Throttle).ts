55d85eb0077bd23ce01277a8fa0cd54aa9aec169
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2023-03-10 04:34:41 +0800

    vault backup: 2023-03-10 04:34:39
    Affected files:
    .obsidian/plugins/obsidian-activity-history/data.json
    TEMP.js
    TEMP.ts
    技术/设计模式/非GoF设计模式/防抖与节流模式(Debounce & Throttle).ts
    技术/语言/JavaScript/call()、apply()、bind().md
    技术/语言/JavaScript/call、apply、bind的区别.md

--- 技术/设计模式/非GoF设计模式/防抖与节流模式(Debounce & Throttle).ts
+++ 技术/设计模式/非GoF设计模式/防抖与节流模式(Debounce & Throttle).ts
@@ -12,18 +12,13 @@ namespace DesignPatterns.DebounceAndThrottle {
    */
   export function debounce(fn: Function, delay: number, immediate = false) {
     let timer: any = null
-    return function (this: any, ...args: any[]) {
+    return function (...args: any[]) {
       if (timer) clearTimeout(timer)
       if (immediate) {
-        if (!timer) fn.apply(this, args)
-        timer = setTimeout(() => {
-          timer = null
-        }, delay)
-
+        if (!timer) fn(args)
+        timer = setTimeout(() => { timer = null }, delay)
       } else {
-        timer = setTimeout(() => {
-          fn.apply(this, args)
-        }, delay)
+        timer = setTimeout(() => { fn(args) }, delay)
       }
     }
   }
@@ -33,4 +28,35 @@ namespace DesignPatterns.DebounceAndThrottle {
   const debounceFn = debounce(fn, 1000, true)
   const interval = setInterval(debounceFn, 100)
   setTimeout(() => { clearInterval(interval) }, 5000)
+
+  /**
+   * 节流: 一段时间内多次触发同一事件, 只执行第一次
+   * @param {Function} fn 要执行的函数
+   * @param {number} delay 延迟时间
+   * @param {boolean} immediate 是否立即执行
+   * @returns {Function} 返回一个新的函数
+   * @example
+   * const fn = () => console.log('fn') // 要执行的函数
+   * const throttleFn = throttle(fn, 1000) // 一秒内多次触发, 只执行第一次
+   * window.addEventListener('scroll', throttleFn)
+   */
+  export function throttle(fn: Function, delay: number, immediate = false) {
+    let timer: any = null
+    return function (...args: any[]) {
+      if (timer) return
+      if (immediate) {
+        fn(args)
+        timer = setTimeout(() => { timer = null }, delay)
+      } else {
+        timer = setTimeout(() => { fn(args) }, delay)
+      }
+    }
+  }
+
+  // 测试
+  const fn2 = () => console.log('fn2')
+  const throttleFn = throttle(fn2, 1000, true)
+  const interval2 = setInterval(throttleFn, 100)
+  setTimeout(() => { clearInterval(interval2) }, 5000)
+
 }
