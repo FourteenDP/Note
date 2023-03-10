1abdc8ed08ea7da19de10d401938da4142479e74
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2023-03-11 00:05:08 +0800

    vault backup: 2023-03-11 00:05:05
    Affected files:
    .gitmodules
    .obsidian/plugins/obsidian-activity-history/data.json
    仓库/Markdown-Compile
    技术/前端开发/其它/package.json 中的坑.md
    技术/数据结构与算法/LRU算法.ts

--- 技术/数据结构与算法/LRU算法.ts
+++ 技术/数据结构与算法/LRU算法.ts
@@ -3,20 +3,25 @@ namespace Algorithm {
     // 缓存容量
     private capacity: number;
     // 缓存
-    private cache: Map<string, number>;
+    private _cache: Map<string, number>;
     constructor(capacity: number) {
       // 初始化容量
       this.capacity = capacity;
       // 初始化缓存
-      this.cache = new Map();
+      this._cache = new Map();
     }
+
+    public get cache() {
+      return this._cache;
+    }
+
     // 获取缓存
     get(key: string): number {
       // 如果缓存中存在该值，则将该值删除并重新添加到缓存中，以保证该值是最近使用的
-      if (this.cache.has(key)) {
-        const value = this.cache.get(key)!;
-        this.cache.delete(key);
-        this.cache.set(key, value);
+      if (this._cache.has(key)) {
+        const value = this._cache.get(key)!;
+        this._cache.delete(key);
+        this._cache.set(key, value);
         return value;
       }
       return -1;
@@ -24,27 +29,27 @@ namespace Algorithm {
     // 添加缓存
     put(key: string, value: number): void {
       // 如果缓存中存在该值，则将该值删除并重新添加到缓存中，以保证该值是最近使用的
-      if (this.cache.has(key)) {
-        this.cache.delete(key);
+      if (this._cache.has(key)) {
+        this._cache.delete(key);
       } else
-      // 如果缓存中不存在该值，且缓存已满，则删除最近最少使用的值
-      if (this.cache.size >= this.capacity) {
-        this.cache.delete(this.cache.keys().next().value);
-      }
+        // 如果缓存中不存在该值，且缓存已满，则删除最近最少使用的值
+        if (this._cache.size >= this.capacity) {
+          this._cache.delete(this._cache.keys().next().value);
+        }
       // 添加缓存
-      this.cache.set(key, value);
+      this._cache.set(key, value);
     }
   }
 
   // 实例化一个容量为2的缓存
   const lru = new LRU(2);
-  lru.put('a', 1);
-  lru.put('b', 2);
-  console.log(lru.get('a'));
-  lru.put('c', 3);
-  console.log(lru.get('b'));
-  lru.put('d', 4);
-  console.log(lru.get('a'));
-  console.log(lru.get('c'));
-  console.log(lru.get('d'));
+  lru.put('a', 1);            // 当前缓存：Map { 'a' => 1 }
+  lru.put('b', 2);            // 当前缓存：Map { 'a' => 1, 'b' => 2 }
+  console.log(lru.get('a'));  // 当前缓存：Map { 'b' => 2, 'a' => 1 }
+  lru.put('c', 3);            // 当前缓存：Map { 'a' => 1, 'c' => 3 }
+  console.log(lru.get('b'));  // 当前缓存：Map { 'a' => 1, 'c' => 3 }
+  lru.put('d', 4);            // 当前缓存：Map { 'c' => 3, 'd' => 4 }
+  console.log(lru.get('a'));  // 当前缓存：Map { 'c' => 3, 'd' => 4 }
+  console.log(lru.get('c'));  // 当前缓存：Map { 'd' => 4, 'c' => 3 }
+  console.log(lru.get('d'));  // 当前缓存：Map { 'c' => 3, 'd' => 4 }
 }
