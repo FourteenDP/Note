291eb3e3656beb61c47cfc4fa96da69725add70d
FourteenD <2899385645@qq.com>
2023-03-10 13:01:21 +0800

    vault backup: 2023-03-10 13:01:18
    Affected files:
    pnpm-lock.yaml
    技术/语言/JavaScript/事件循环（Event Loop）.ts

--- 技术/语言/JavaScript/事件循环（Event Loop）.ts
+++ 技术/语言/JavaScript/事件循环（Event Loop）.ts
@@ -1,27 +1,27 @@
 console.log("1");
 setTimeout(()=>{
-    console.log(2)
+    console.log(7)
     Promise.resolve().then(()=>{
-        console.log(3);
+        console.log(8);
         process.nextTick(function foo() {
-            console.log(4);
+            console.log(9);
         });
     })
 })
 Promise.resolve().then(()=>{
     console.log(5);
     setTimeout(()=>{
-        console.log(6)
+        console.log(10)
     })
     Promise.resolve().then(()=>{
-        console.log(7);
+        console.log(6);
     })
 })
 
 process.nextTick(function foo() {
-    console.log(8);
+    console.log(3);
     process.nextTick(function foo() {
-        console.log(9);
+        console.log(4);
     });
 });
-console.log("10")
+console.log("2")
