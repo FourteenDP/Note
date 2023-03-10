9ef8d9703b1083949d8d88ffc1d30a20d92dc464
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2023-03-10 14:21:48 +0800

    vault backup: 2023-03-10 14:21:46
    Affected files:
    技术/数据结构与算法/LRU算法.ts

---
+++ 技术/数据结构与算法/LRU算法.ts
@@ -0,0 +1,27 @@
+namespace Algorithm {
+    export class LRU {
+        private capacity: number;
+        private cache: Map<string, number>;
+        constructor(capacity: number) {
+            this.capacity = capacity;
+            this.cache = new Map();
+        }
+        get(key: string): number {
+            if (this.cache.has(key)) {
+                const value = this.cache.get(key);
+                this.cache.delete(key);
+                this.cache.set(key, value);
+                return value;
+            }
+            return -1;
+        }
+        put(key: string, value: number): void {
+            if (this.cache.has(key)) {
+                this.cache.delete(key);
+            } else if (this.cache.size >= this.capacity) {
+                this.cache.delete(this.cache.keys().next().value);
+            }
+            this.cache.set(key, value);
+        }
+    }
+}
