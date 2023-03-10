7e401216e8a56b33f96ebfaad0f567c6e31f52c1
FourteenD <2899385645@qq.com>
2023-03-10 11:52:13 +0800

    vault backup: 2023-03-10 11:52:12
    Affected files:
    技术/语言/JavaScript/call、apply、bind手写.ts

--- 技术/语言/JavaScript/call、apply、bind手写.ts
+++ 技术/语言/JavaScript/call、apply、bind手写.ts
@@ -1,35 +1,38 @@
-interface Function {
-  myCall(context: any, ...args: any[]): any
-  apply(context: any, args: any[]): any
-  bind(context: any, ...args: any[]): any
-}
-
-// 实现call方法
-Function.prototype.myCall = function (context: any, ...args: any[]) {
-  // 判断是否为函数调用
-  if (typeof this !== 'function') {
-    throw new TypeError('Error')
+namespace DesignPatterns.This {
+  interface Function {
+    myCall(context: any, ...args: any[]): any
+    apply(context: any, args: any[]): any
+    bind(context: any, ...args: any[]): any
   }
-  // 判断context是否为null或undefined (null和undefined都会被转换成globalThis)
-  context = context || globalThis
-  // 将调用call的函数设为context的一个属性
-  context.fn = this as any
-  // 调用该函数
-  const result = context.fn(...args)
-  delete context.fn
-  return result
-}
 
-// 测试
-const obj = {
-  name: 'obj',
-  getName() {
-    console.log(this.name)
+  // 实现call方法
+  Function.prototype.myCall = function (context: any, ...args: any[]) {
+    // 判断是否为函数调用
+    if (typeof this !== 'function') {
+      throw new TypeError('Error')
+    }
+    // 判断context是否为null或undefined (null和undefined都会被转换成globalThis)
+    context = context || globalThis
+    // 将调用call的函数设为context的一个属性
+    context.fn = this as any
+    // 调用该函数
+    const result = context.fn(...args)
+    delete context.fn
+    return result
   }
-}
 
-const obj2 = {
-  name: 'obj2'
-}
+  // 测试
+  const obj = {
+    name: 'obj',
+    getName() {
+      console.log(this.name)
+    }
+  }
 
-obj.getName.myCall(obj2) // obj2
+  const obj2 = {
+    name: 'obj2'
+  }
+
+  obj.getName.myCall(obj2) // obj2
+
+}
