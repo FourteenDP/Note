820df776ada7b62b111933636250deba68c4cd6e
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2023-05-07 17:53:45 +0800

    update

--- .gitmodules.init.sh
+++ .gitmodules.init.sh
@@ -1,7 +1,8 @@
-git submodule add git@github.com:FourteenDP/Vue3-Hello.git 仓库/Vue3-Hello
-git submodule add git@github.com:FourteenDP/Script.git 仓库/Script
-git submodule add git@github.com:FourteenDP/Code-Snippets.git 仓库/Code-Snippets
-git submodule add git@github.com:FourteenDP/CS-Hello.git 仓库/CS-Hello
-git submodule add git@github.com:FourteenDP/Markdown-Compile.git 仓库/Markdown-Compile
-git submodule add git@github.com:FourteenDP/-.git 拾肆
-git submodule add git@github.com:FourteenDP/--.git 日志
+git submodule add git@github.com:FourteenDP/Vue3-Hello.git "./仓库/Vue3-Hello" --force
+git submodule add git@github.com:FourteenDP/Script.git "./仓库/Script" --force
+git submodule add git@github.com:FourteenDP/Code-Snippets.git "./仓库/Code-Snippets" --force
+git submodule add git@github.com:FourteenDP/CS-Hello.git "./仓库/CS-Hello" --force
+git submodule add git@github.com:FourteenDP/Markdown-Compile.git "./仓库/Markdown-Compile" --force
+git submodule add git@github.com:FourteenDP/Me-Config.git "./仓库/Me-Config" --force
+git submodule add git@github.com:FourteenDP/--.git "./日志" --force
+git submodule add git@github.com:FourteenDP/-.git "./拾肆" --force
