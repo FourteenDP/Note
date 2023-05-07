09963239223e129c8d4b11ca425dc7790b8c17a2
FourteenDP <53899166+FourteenDP@users.noreply.github.com>
2023-05-07 16:14:22 +0800

    new file:   .gitmodules.init.sh

---
+++ .gitmodules.init.sh
@@ -0,0 +1,7 @@
+git submodule add git@github.com:FourteenDP/Vue3-Hello.git 仓库/Vue3-Hello
+git submodule add git@github.com:FourteenDP/Script.git 仓库/Script
+git submodule add git@github.com:FourteenDP/Code-Snippets.git 仓库/Code-Snippets
+git submodule add git@github.com:FourteenDP/CS-Hello.git 仓库/CS-Hello
+git submodule add git@github.com:FourteenDP/Markdown-Compile.git 仓库/Markdown-Compile
+git submodule add git@github.com:FourteenDP/-.git 拾肆
+git submodule add git@github.com:FourteenDP/--.git 日志
