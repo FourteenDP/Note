06ec7efa6d042b0e347f84281bc5bd7fc2462036
FourteenDP <fourteendp@qq.com>
2024-01-23 10:39:55 +0800

    up

--- .gitmodules.init.sh
+++ .gitmodules.init.sh
@@ -1,8 +1,2 @@
-git submodule add git@github.com:FourteenDP/Vue3-Hello.git "./仓库/Vue3-Hello" --force
-git submodule add git@github.com:FourteenDP/Script.git "./仓库/Script" --force
-git submodule add git@github.com:FourteenDP/Code-Snippets.git "./仓库/Code-Snippets" --force
-git submodule add git@github.com:FourteenDP/CS-Hello.git "./仓库/CS-Hello" --force
-git submodule add git@github.com:FourteenDP/Markdown-Compile.git "./仓库/Markdown-Compile" --force
-git submodule add git@github.com:FourteenDP/Me-Config.git "./仓库/Me-Config" --force
 git submodule add git@github.com:FourteenDP/--.git "./日志" --force
 git submodule add git@github.com:FourteenDP/-.git "./拾肆" --force
